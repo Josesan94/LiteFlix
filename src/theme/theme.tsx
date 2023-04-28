bafe82c1443cd08e3835e723526b92766d62ba0b
Josesan94 <josesantillan2@hotmai>
2023-04-28 17:20:31 -0300

    configure colors theme palette

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,11 +1,44 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: React.CSSProperties['color'];
+    };
+  }
+
+  interface Palette {
+    neutral: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    neutral: PaletteOptions['primary'];
+  }
+
+  interface PaletteColor {
+    darker?: string;
+  }
+
+  interface SimplePaletteColorOptions {
+    darker?: string;
+  }
+
+  interface ThemeOptions {
+    status: {
+      danger: React.CSSProperties['color'];
+    };
+  }
+}
+
 // A custom theme for this app
 const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#FFFFFF',
     },
     secondary: {
       main: '#64EEBC',
@@ -13,6 +46,9 @@ const theme = createTheme({
     error: {
       main: red.A400,
     },
+    neutral: {
+      main:"#242424"
+    }
   },
   typography: {
     fontFamily: "'Bebas Neue', sans-serif",
