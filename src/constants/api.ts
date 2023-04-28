b3258383af0da9aa1d74f56e8a53af91c449d2e5
Josesan94 <josesantillan2@hotmai>
2023-04-28 11:22:59 -0300

    create api constants file to handle endpoints urls and apikeys

---
+++ src/constants/api.ts
@@ -0,0 +1,6 @@
+
+export const BASE_API_URL = 'https://api.themoviedb.org/3/movie';
+export const OUTSTANDING_MOVIE_API = `${BASE_API_URL}/now_playing`;
+export const MOST_POPULAR_API = `${BASE_API_URL}/popular`;
+export const IMAGE_URL = 'https://image.tmdb.org/t/p';
+export const API_KEY = '6f26fd536dd6192ec8a57e94141f8b20'
