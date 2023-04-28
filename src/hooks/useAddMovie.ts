7e89be2df7ded72f837b0a602587e56a8e7ad7b5
Josesan94 <josesantillan2@hotmai>
2023-04-28 11:38:22 -0300

    create custom hook to handle logic for adding new movies to "my list" catalog

---
+++ src/hooks/useAddMovie.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { PopularMovie } from "../types/movies";
+
+
+function useAddMovie() {
+  const [storedMovies, setStoredMovies] = useState<PopularMovie[]>([]);
+
+  const addMovie = (movie: PopularMovie) => {
+    const newList = [...storedMovies, movie];
+
+    setStoredMovies(newList);
+    localStorage.setItem("createdMovies", JSON.stringify(newList));
+  };
+
+  useEffect(() => {
+    const createdMovies = localStorage.getItem("createdMovies");
+
+    if (createdMovies !== null) {
+      const parsedMovies = JSON.parse(createdMovies);
+
+      setStoredMovies(parsedMovies);
+    }
+  }, []);
+
+  return {storedMovies, addMovie};
+}
+
+export default useAddMovie;
