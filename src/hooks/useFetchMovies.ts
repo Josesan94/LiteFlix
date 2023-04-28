24bef7a5ba9e71dcbbd45b7edd9542ba1c5d9f27
Josesan94 <josesantillan2@hotmai>
2023-04-28 11:37:38 -0300

    Create custom hook that handles  endpoint data fetching for popular movies and outstanding movies, and setting to state

---
+++ src/hooks/useFetchMovies.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Movie, PopularMovie } from "../types/movies";
+import {
+  API_KEY,
+  OUTSTANDING_MOVIE_API,
+  MOST_POPULAR_API,
+} from "../constants/api";
+
+function useFetchMovies() {
+  const [outstandingMovie, setOutstadingMovie] = useState<Movie | null>(null);
+  const [popularMovies, setPopularMovies] = useState<PopularMovie[]>([]);
+
+  const getMovies = async () => {
+    try {
+      const outstadingResponse = await axios.get(
+        `${OUTSTANDING_MOVIE_API}?api_key=${API_KEY}`
+      );
+      const outstandingMoviesData = outstadingResponse.data.results[outstadingResponse.data.results.length - 5];
+
+      const popularResponse = await axios.get(
+        `${MOST_POPULAR_API}?api_key=${API_KEY}`
+      )
+      const popularMoviesData = popularResponse.data.results.slice(5, 9);
+
+      setOutstadingMovie(outstandingMoviesData);
+      setPopularMovies(popularMoviesData);
+    } catch (err) {
+      console.log("An error has ocurred", err);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return { outstandingMovie, popularMovies };
+}
+
+export default useFetchMovies;
