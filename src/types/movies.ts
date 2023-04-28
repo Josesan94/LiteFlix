a5606f3f802c2a6c20c69ce76472f5a41c783e99
Josesan94 <josesantillan2@hotmai>
2023-04-28 11:22:30 -0300

    create interfaces for endpoint data

--- src/types/movies.ts
+++ src/types/movies.ts
@@ -1,24 +1,35 @@
 export interface Movie {
-    adult:             boolean;
-    backdrop_path:     string;
-    genre_ids:         number[];
-    id:                number;
-    original_language: OriginalLanguage;
-    original_title:    string;
-    overview:          string;
-    popularity:        number;
-    poster_path:       string;
-    release_date:      Date;
-    title:             string;
-    video:             boolean;
-    vote_average:      number;
-    vote_count:        number;
-   }
+  id: number;
+  title: string;
+  poster_path: string;
+}
 
+export interface PopularMovie {
+  id: number;
+  title: string;
+  backdrop_path: string;
+}
+
+export interface MovieEndpoint {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: OriginalLanguage;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: Date;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
 export enum OriginalLanguage {
-En = "en",
-Es = "es",
-Ko = "ko",
-Nl = "nl",
+  En = "en",
+  Es = "es",
+  Ko = "ko",
+  Nl = "nl",
 }
