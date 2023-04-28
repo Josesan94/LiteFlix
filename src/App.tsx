aa782adffee05cd7dd9d3244d3dc37019b5968ec
Josesan94 <josesantillan2@hotmai>
2023-04-28 11:41:19 -0300

    add styles to homepage and integrate created components with logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-
+import { Container, Typography } from "@mui/material";
+import MoviesList from "./components/Movies/MoviesList";
+import useFetchMovies from "./hooks/useFetchMovies";
+import { IMAGE_URL } from "./constants/api";
+import NavBar from "./components/navBar";
 
 function App() {
+  const { outstandingMovie } = useFetchMovies();
+
+  const outStandingImage = `${IMAGE_URL}/w500${outstandingMovie?.poster_path}`;
 
   return (
     <>
-      <div>
-        hello
-      </div>
+      <Container
+        maxWidth='xl'
+        style={{paddingLeft:'50px', paddingRight:'50px'}}
+      >
+        <img
+        src={outStandingImage}
+        alt="background"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1,
+          objectFit: 'cover',
+        }}
+        />
+        <NavBar/>
+        <Typography variant="h6">Ver: populares</Typography>
+        <MoviesList listType="popularMovies" />
+        <MoviesList listType="myMovies" />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
