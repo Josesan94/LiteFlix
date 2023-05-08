184bba326a6a93ee3c3d9af6b0a26da6d240c204
Josesan94 <josesantillan2@hotmai>
2023-05-08 18:40:04 -0300

    fix paddings in Container

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import { Container } from "@mui/material";
 import Homepage from "./pages/Homepage";
 
 function App() {
-
-
   return (
     <>
       <Container
         sx={{
           maxWidth: "2100px !important",
           paddingY: "25px !important",
-          paddingX: "70px !important",
+          paddingX: { xs: "20px !important", md: "70px !important" },
         }}
       >
-      <Homepage/>
+        <Homepage />
       </Container>
     </>
   );
