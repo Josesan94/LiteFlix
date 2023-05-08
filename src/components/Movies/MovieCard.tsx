8ba85fb0a38c17c9e79b2ec23adc685e17004067
Josesan94 <josesantillan2@hotmai>
2023-05-08 18:44:59 -0300

    adapt movie card to mobile sizes and give style to hover

--- src/components/Movies/MovieCard.tsx
+++ src/components/Movies/MovieCard.tsx
@@ -1,7 +1,13 @@
-import React, {useState} from "react";
-import { Card, CardContent, IconButton, Typography, Stack } from '@mui/material';
-import { PlayArrow, Star } from '@mui/icons-material';
-import { styled } from '@mui/system';
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  IconButton,
+  Typography,
+  Stack,
+} from "@mui/material";
+import { PlayArrow, Star } from "@mui/icons-material";
+import { styled } from "@mui/system";
 import { PopularMovie } from "../../types/movies";
 import { IMAGE_URL } from "../../constants/api";
 
@@ -9,7 +15,7 @@ type Props = {
   movie: PopularMovie;
 };
 
-const StyledCard = styled(Card)(({ theme }) => ({
+const StyledCard = styled(Card)(() => ({
   position: "relative",
   backgroundSize: "cover",
   backgroundPosition: "center",
@@ -18,7 +24,7 @@ const StyledCard = styled(Card)(({ theme }) => ({
   height: 150,
 }));
 
-const StyledCardContent = styled(CardContent)(({ theme }) => ({
+const StyledCardContent = styled(CardContent)(() => ({
   position: "absolute",
   bottom: 0,
   left: 0,
@@ -29,8 +35,7 @@ const StyledCardContent = styled(CardContent)(({ theme }) => ({
   transition: "opacity 0.3s",
 }));
 
-
-const HoverCardContent:any = styled(CardContent)(({ theme }) => ({
+const HoverCardContent = styled(CardContent)(() => ({
   position: "absolute",
   top: 0,
   bottom: 0,
@@ -45,65 +50,111 @@ const HoverCardContent:any = styled(CardContent)(({ theme }) => ({
   transition: "opacity 0.3s",
 }));
 
-
 const MovieCard: React.FC<Props> = ({ movie }) => {
   const image = `${IMAGE_URL}/w500${movie.backdrop_path}`;
   const [isHovering, setIsHovering] = useState<boolean>(false);
 
-
-  const year = movie.release_date.split("-")[0]
-
-
-
-
+  const year = movie.release_date?.split("-")[0];
 
   return (
-    <StyledCard style={{ backgroundImage: `url(${image})`}}
-    onMouseEnter={() => setIsHovering(true)}
+    <StyledCard
+      style={{ backgroundImage: `url(${image})` }}
+      onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
     >
       <StyledCardContent style={{ opacity: isHovering ? 0 : 1 }}>
-        <Stack  alignItems={'center'} justifyContent={'flex-end'} spacing={2}> 
-        <IconButton style={{color:"rgba(255, 255, 255, 1)", border:"1px solid #FFFFFF" , background:"rgba(36, 36, 36, 0.5)"}} aria-label="play">
-          <PlayArrow/>
-        </IconButton>
-        <Typography color={'rgba(255, 255, 255, 1)'} letterSpacing={'4px'} fontWeight={'400'} fontSize={'16'} lineHeight={'16px'} >
+        <Stack alignItems={"center"} justifyContent={"flex-end"} spacing={2}>
+          <IconButton
+            style={{
+              color: "rgba(255, 255, 255, 1)",
+              border: "1px solid #FFFFFF",
+              background: "rgba(36, 36, 36, 0.5)",
+            }}
+            aria-label="play"
+          >
+            <PlayArrow />
+          </IconButton>
+          <Typography
+            color={"rgba(255, 255, 255, 1)"}
+            letterSpacing={"4px"}
+            fontWeight={"400"}
+            fontSize={"16"}
+            lineHeight={"16px"}
+          >
             {movie.title}
-        </Typography>
+          </Typography>
         </Stack>
       </StyledCardContent>
       <HoverCardContent style={{ opacity: isHovering ? 1 : 0 }}>
-        <Stack width={'100%'} mt={9} direction={'column'} alignItems={'center'} justifyContent={'flex-end'} gap={2}>
-        <Stack width={'100%'} direction={"row"} alignItems={"center"} justifyContent={'flex-start'} spacing={1}>
-        <IconButton 
-        size="small"
-        sx={{
-          width:'24px',
-          height:'24px',
-          color: 'rgba(255, 255, 255, 1)',
-          border: '1px solid #FFFFFF',
-          background: 'rgba(36, 36, 36, 0.5)',
-          '&:hover': {
-            backgroundColor: 'secondary.main',
-            color: '#000000',
-          },
-        }}
-         aria-label="play">
-          <PlayArrow/>
-        </IconButton>
-        <Typography color={'rgba(255, 255, 255, 1)'} letterSpacing={'4px'} fontWeight={'400'} fontSize={'16px'} lineHeight={'16px'} >
-            {movie.title}
-        </Typography>
+        <Stack
+          width={"100%"}
+          mt={9}
+          direction={"column"}
+          alignItems={"center"}
+          justifyContent={"flex-end"}
+          gap={2}
+        >
+          <Stack
+            width={"100%"}
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"flex-start"}
+            spacing={1}
+          >
+            <IconButton
+              size="small"
+              sx={{
+                width: "24px",
+                height: "24px",
+                color: "rgba(255, 255, 255, 1)",
+                border: "1px solid #FFFFFF",
+                background: "rgba(36, 36, 36, 0.5)",
+                "&:hover": {
+                  backgroundColor: "secondary.main",
+                  color: "#000000",
+                },
+              }}
+              aria-label="play"
+            >
+              <PlayArrow />
+            </IconButton>
+            <Typography
+              color={"rgba(255, 255, 255, 1)"}
+              letterSpacing={"4px"}
+              fontWeight={"400"}
+              fontSize={"16px"}
+              lineHeight={"16px"}
+            >
+              {movie.title}
+            </Typography>
+          </Stack>
+          <Stack
+            width={"100%"}
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Stack direction={"row"} alignItems={"center"}>
+              <Star color={"secondary"} />
+              <Typography
+                color={"primary"}
+                fontSize={"14px"}
+                lineHeight={"12px"}
+                letterSpacing={"2px"}
+              >
+                {movie.vote_average}
+              </Typography>
+            </Stack>
+            <Typography
+              color={"primary"}
+              fontSize={"14px"}
+              lineHeight={"12px"}
+              letterSpacing={"2px"}
+            >
+              {year}
+            </Typography>
+          </Stack>
         </Stack>
-        <Stack width={'100%'} direction={"row"} alignItems={"center"} justifyContent={'space-between'}>
-        <Stack direction={'row'} alignItems={'center'}>
-        <Star color={"secondary"} />
-        <Typography color={'primary'} fontSize={'14px'} lineHeight={'12px'} letterSpacing={'2px'}>{movie.vote_average}</Typography>
-        </Stack>
-        <Typography color={'primary'} fontSize={'14px'} lineHeight={'12px'} letterSpacing={'2px'}>{year}</Typography>
-        </Stack>
-        </Stack>
-        
       </HoverCardContent>
     </StyledCard>
   );
